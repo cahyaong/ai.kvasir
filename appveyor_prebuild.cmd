c340d952a6d638a56a684a38bd30d1b7c15a7a8b
Cahya Ong <cahya.ong@gmail.com>
2018-12-04 23:50:15 +1100

    Added Paket references file to all projects.

--- appveyor_prebuild.cmd
+++ appveyor_prebuild.cmd
@@ -1,14 +1,9 @@
-ECHO OFF
-
-SETLOCAL
-SET PATH=%PATH%;%~dp0Tools\NuGet
-
 ECHO ON
 
 git submodule -q update --init --recursive
 
 cd .\Source
 .\.paket\paket.exe restore
-cd ..
 
-nuget restore External\cop.core\Source\nGratis.Cop.Core.sln
+cd ..\External\cop.core
+.\appveyor_prebuild.cmd
