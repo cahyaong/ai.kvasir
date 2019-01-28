3b2c6ff09694c1bf587d40dfb4dcf4fecddef712
Cahya Ong <cahya.ong@gmail.com>
2019-01-28 15:37:20 +1100

    Added <GameContext> with basic player initialization step.

---
+++ Source/Kvasir.Core.Test/Engine/GameContextTests.cs
@@ -0,0 +1,111 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GameContextTests.cs" company="nGratis">
+//  The MIT License (MIT)
+//
+//  Copyright (c) 2014 - 2018 Cahya Ong
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+// </copyright>
+// <author>Cahya Ong - cahya.ong@gmail.com</author>
+// <creation_timestamp>Monday, 28 January 2019 4:07:09 AM UTC</creation_timestamp>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace nGratis.AI.Kvasir.Core.Test
+{
+    using FluentAssertions;
+    using FluentAssertions.Execution;
+    using nGratis.AI.Kvasir.Contract;
+    using Xunit;
+
+    public class GameContextTests
+    {
+        public class Constructor
+        {
+            [Fact]
+            public void WhenGettingValidParameter_ShouldSetCurrentPhaseToBeginning()
+            {
+                // Arrange.
+
+                var firstPlayer = new Player();
+                var secondPlayer = new Player();
+
+                var randomGenerator = new RandomGenerator();
+
+                // Act.
+
+                var gameContext = new GameContext(firstPlayer, secondPlayer, randomGenerator);
+
+                // Assert.
+
+                gameContext
+                    .CurrentPhase
+                    .Should().Be(GameContext.Phase.Beginning);
+            }
+
+            [Fact]
+            public void WhenGettingValidParameter_ShouldSetupPlayer()
+            {
+                // Arrange.
+
+                var firstPlayer = new Player();
+                var secondPlayer = new Player();
+
+                var randomGenerator = new RandomGenerator();
+
+                // Act.
+
+                var gameContext = new GameContext(firstPlayer, secondPlayer, randomGenerator);
+
+                // Assert.
+
+                gameContext
+                    .FirstPlayer
+                    .Should().NotBeNull();
+
+                gameContext
+                    .SecondPlayer
+                    .Should().NotBeNull();
+
+                if (gameContext.FirstPlayer.HasPriority)
+                {
+                    gameContext
+                        .SecondPlayer.HasPriority
+                        .Should().BeFalse();
+                }
+                else
+                {
+                    gameContext
+                        .SecondPlayer.HasPriority
+                        .Should().BeTrue();
+                }
+
+                using (new AssertionScope())
+                {
+                    gameContext
+                        .FirstPlayer.Life
+                        .Should().Be(20);
+
+                    gameContext
+                        .SecondPlayer.Life
+                        .Should().Be(20);
+                }
+            }
+        }
+    }
+}
---
+++ Source/Kvasir.Core/Engine/GameContext.cs
@@ -0,0 +1,147 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="GameContext.cs" company="nGratis">
+//  The MIT License (MIT)
+//
+//  Copyright (c) 2014 - 2018 Cahya Ong
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+// </copyright>
+// <author>Cahya Ong - cahya.ong@gmail.com</author>
+// <creation_timestamp>Wednesday, 23 January 2019 10:45:26 AM UTC</creation_timestamp>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace nGratis.AI.Kvasir.Core
+{
+    using System.Diagnostics.CodeAnalysis;
+    using nGratis.AI.Kvasir.Contract;
+    using nGratis.Cop.Core.Contract;
+    using Stateless;
+
+    public class GameContext
+    {
+        public enum Phase
+        {
+            Unknown = 0,
+
+            Beginning,
+            Playing,
+            Ending
+        }
+
+        private enum Action
+        {
+            [SuppressMessage("ReSharper", "UnusedMember.Local")]
+            Unknown = 0,
+
+            Begin,
+            PlayTurn,
+            End
+        }
+
+        private readonly StateMachine<Phase, Action> _stateMachine;
+
+        private readonly IRandomGenerator _randomGenerator;
+
+        public GameContext(Player firstPlayer, Player secondPlayer, IRandomGenerator randomGenerator)
+        {
+            Guard
+                .Require(firstPlayer, nameof(firstPlayer))
+                .Is.Not.Null();
+
+            Guard
+                .Require(secondPlayer, nameof(secondPlayer))
+                .Is.Not.Null();
+
+            Guard
+                .Require(randomGenerator, nameof(randomGenerator))
+                .Is.Not.Null();
+
+            this.FirstPlayer = firstPlayer;
+            this.SecondPlayer = secondPlayer;
+
+            this._randomGenerator = randomGenerator;
+            this._stateMachine = new StateMachine<Phase, Action>(Phase.Unknown);
+
+            this._stateMachine
+                .Configure(Phase.Unknown)
+                .Permit(Action.Begin, Phase.Beginning);
+
+            this._stateMachine
+                .Configure(Phase.Beginning)
+                .OnActivate(this.OnBeginningActivated)
+                .Permit(Action.PlayTurn, Phase.Playing);
+
+            this._stateMachine
+                .Configure(Phase.Playing)
+                .OnActivate(this.OnPlayingActivated)
+                .Permit(Action.End, Phase.Ending);
+
+            this._stateMachine
+                .Configure(Phase.Ending)
+                .OnActivate(this.OnEndingActivated);
+
+            this._stateMachine.Fire(Action.Begin);
+        }
+
+        public Phase CurrentPhase => this._stateMachine.State;
+
+        public Player FirstPlayer { get; }
+
+        public Player SecondPlayer { get; }
+
+        private void OnBeginningActivated()
+        {
+            this.SetupPlayer();
+        }
+
+        private void OnPlayingActivated()
+        {
+        }
+
+        private void OnEndingActivated()
+        {
+        }
+
+        private void SetupPlayer()
+        {
+            var firstValue = 0;
+            var secondValue = 0;
+
+            while (firstValue == secondValue)
+            {
+                firstValue = this._randomGenerator.RollDice(20);
+                secondValue = this._randomGenerator.RollDice(20);
+            }
+
+            if (firstValue > secondValue)
+            {
+                this.FirstPlayer.HasPriority = true;
+                this.SecondPlayer.HasPriority = false;
+            }
+            else
+            {
+                this.FirstPlayer.HasPriority = false;
+                this.SecondPlayer.HasPriority = true;
+            }
+
+            this.FirstPlayer.Life = 20;
+            this.SecondPlayer.Life = 20;
+        }
+    }
+}
